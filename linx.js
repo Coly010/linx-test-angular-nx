65c4261cea19026aa6db0478c89fe18037d83824
Colum Ferry <cferry09@gmail.com>
2024-02-26 21:04:29 +0000

    Update README

---
+++ linx.js
@@ -0,0 +1,26 @@
+const child_process = require("node:child_process");
+
+if(process.argv.length === 2) {
+    throw new Error("[LINX] - You did not specify the nx command to run.")
+}
+
+async function main() {
+    const nxCommandToRun = process.argv.slice(2).join(" ");
+    const commandToRun = `docker run -v .:/usr/src/app -p 4200:4200 cferry101/linx ${nxCommandToRun}`;
+    console.log(commandToRun)
+    const promise = new Promise((res, rej) => {
+        const cp = child_process.exec(commandToRun, (error, stdout, stderr) => {
+            if(error) {
+                console.error(error);
+            }
+            console.log(stdout);
+            console.error(stderr)
+        });
+        cp.stdio[1]?.on('data', (data) => {
+            console.log(data)
+        })
+    });
+    await promise;
+}
+
+main();
